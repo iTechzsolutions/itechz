b9fcbb894799e4c2bf323d9838c070edc781bda6
iTechzsolutions <itechz1solutions@gmail.com>
2026-04-04 15:40:42 +0530

    Update shared.js

--- shared.js
+++ shared.js
@@ -6,6 +6,10 @@ const NAV_HTML = `
     <a class="logo" href="index.html">
   <img src="Logo.png" alt="iTechz Logo">
     </a>
+    <a class="logo" href="index.html">
+  <img src="images/logo.png" alt="logo">
+  <span>iTechz Solutions</span>
+    </a>
     <ul class="nav-links">
       <li><a href="index.html" data-page="index">Home</a></li>
       <li><a href="services.html" data-page="services">Services</a></li>
